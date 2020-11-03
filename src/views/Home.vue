67ad75c5d894d3779918ebc5fd3cd7495f437557
xMonoKuma <36600556+iTdiV@users.noreply.github.com>
2020-11-03 14:39:36 +0700

    Upload Project Vue

---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <HeaderHijrah />
+    
+    <HeroHijrah />
+
+    <BannerHijrah />
+
+    <InstaHijrah />
+
+    <PartnerHijrah />
+
+    <FooterHijrah />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderHijrah from '@/components/HeaderHijrah.vue'
+
+import HeroHijrah from '@/components/HeroHijrah.vue'
+
+import BannerHijrah from '@/components/BannerHijrah.vue'
+
+import InstaHijrah from '@/components/InstaHijrah.vue'
+
+import PartnerHijrah from '@/components/PartnerHijrah.vue'
+
+import FooterHijrah from '@/components/FooterHijrah.vue'
+
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderHijrah,
+    HeroHijrah,
+    BannerHijrah,
+    InstaHijrah,
+    PartnerHijrah,
+    FooterHijrah
+    
+  }
+}
+</script>
